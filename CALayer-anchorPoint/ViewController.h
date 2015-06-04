eedd9a62b0555287bd2a27ef4c06c098b7a57814
liuyujing <liuyujingmusic@163.com>
2015-06-04 18:53:34 +0800

    Initial Commit

---
+++ CALayer-anchorPoint/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CALayer-anchorPoint
+//
+//  Created by Bruce on 15/6/4.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CALayer-anchorPointTests/CALayer_anchorPointTests.m
@@ -0,0 +1,40 @@
+//
+//  CALayer_anchorPointTests.m
+//  CALayer-anchorPointTests
+//
+//  Created by Bruce on 15/6/4.
+//  Copyright (c) 2015年 Bruce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface CALayer_anchorPointTests : XCTestCase
+
+@end
+
+@implementation CALayer_anchorPointTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
